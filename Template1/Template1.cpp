ca8121ddc262185ab5220d0eabdda54bfb42ca96
Maria Gryglewska <maria.gryglewska51@gmail.com>
2020-10-27 17:53:14 +0100

    rotation around centre of mass and around space of orgin

--- Template1/Template1.cpp
+++ Template1/Template1.cpp
@@ -7,12 +7,25 @@
 #include "include\GL\GLU.H"
 #include "include\GL\glut.h"
 
-	//glColor4f(1.0f, 0.0f, 0.0f, 1.0f);//Red;
-	//glColor4f(0.0f, 1.0f, 0.0f, 1.0f);//Green
-	//glColor4f(0.0f, 0.0f, 1.0f, 1.0f);//Blue;
-	//glColor4f(1.0f, 0.5f, 0.0f, 1.0f);//orange
-	//glColor4f(1.0f, 0.0f, 1.0f, 1.0f);//magenta
-	//glColor4f(1.0f, 1.0f, 0.0f, 1.0f);//yellow
+//clors//
+//glColor4f(1.0f, 0.0f, 0.0f, 1.0f);//Red;
+//glColor4f(0.0f, 1.0f, 0.0f, 1.0f);//Green
+//glColor4f(0.0f, 0.0f, 1.0f, 1.0f);//Blue;
+//glColor4f(1.0f, 0.5f, 0.0f, 1.0f);//Orange
+//glColor4f(1.0f, 0.0f, 1.0f, 1.0f);//Magenta
+//glColor4f(1.0f, 1.0f, 0.0f, 1.0f);//Yellow
+
+
+const double lengthOfTriangle = 100.0f;
+
+
+double angle = 0;
+
+void CenterOfMassRotation() {
+	glTranslated(lengthOfTriangle / 3, lengthOfTriangle / 3, 0);
+	glRotated(angle, 0, 0, 1);
+	glTranslated(-lengthOfTriangle / 3, -lengthOfTriangle / 3, 0);
+}
 
 void DrawTriangle(GLfloat red, GLfloat green, GLfloat blue, GLfloat alpha) {
 	glBegin(GL_POLYGON);// OpenGLa state
@@ -26,87 +39,59 @@ void DrawTriangle(GLfloat red, GLfloat green, GLfloat blue, GLfloat alpha) {
 
 }
 void DrawQuarter(void) {
+	glPushMatrix();
 	glTranslated(0, 0, 0);
+	if (true) CenterOfMassRotation();
 	DrawTriangle(0.0f, 0.0f, 1.0f, 1.0f);
+	glPopMatrix();
+
 	glTranslated(0, 100, 0);
+
+	glPushMatrix();
+	if (true) CenterOfMassRotation();
 	DrawTriangle(0.0f, 1.0f, 0.0f, 1.0f);
+	glPopMatrix();
+
 	glTranslated(0, 100, 0);
+	glPushMatrix();
+	if (true) CenterOfMassRotation();
 	DrawTriangle(1.0f, 0.0f, 0.0f, 1.0f);
+	glPopMatrix();
+
 	glTranslated(100, -100, 0);
+	glPushMatrix();
+	if (true) CenterOfMassRotation();
 	DrawTriangle(1.0f, 1.0f, 0.0f, 1.0f);
+	glPopMatrix();
+
 	glTranslated(0, -100, 0);
+	glPushMatrix();
+	if (true) CenterOfMassRotation();
 	DrawTriangle(1.0f, 0.5f, 0.0f, 1.0f);
+	glPopMatrix();
+
 	glTranslated(100, 0, 0);
+	glPushMatrix();
+	if (true) CenterOfMassRotation();
 	DrawTriangle(1.0f, 0.0f, 1.0f, 1.0f);
+	glPopMatrix();
+	//
 
 }
 
-/*void DrawTriangleBlue(void) {
-	glBegin(GL_POLYGON);// OpenGL’a state
-	glColor4f(0.0f, 0.0f, 1.0f, 1.0f);//blue
-	glVertex2f(0.0f, 100.0f);
-	glColor4f(0.0f, 0.0f, 1.0f, 1.0f);//blue
-	glVertex2f(0.0f, 0.0f);
-	glColor4f(0.0f, 0.0f, 1.0f, 1.0f);//Blue
-	glVertex2f(100.0f, 0.0f);
-	glEnd();
+
+
+void Timer(int) {
+	angle += 2;
+	glutPostRedisplay();
+	//glutTimerFunc(1000, timer, 0);
 }
 
-//void DrawTriangleGreen(void) {
-	glBegin(GL_POLYGON);// OpenGL’a state
-	glColor4f(0.0f, 1.0f, 0.0f, 1.0f);//Green
-	glVertex2f(0.0f, 100.0f);
-	glColor4f(0.0f, 1.0f, 0.0f, 1.0f);//Green
-	glVertex2f(0.0f, 0.0f);
-	glColor4f(0.0f, 1.0f, 0.0f, 1.0f);//Green
-	glVertex2f(100.0f, 0.0f);
-	glEnd();
-}
+void QuarterMovment(void) {
+	glRotated(angle, 0, 0, 1);
+	DrawQuarter();
 
-void DrawTriangleOrange(void) {
-	glBegin(GL_POLYGON);// OpenGL’a state
-	glColor4f(1.0f, 0.5f, 0.0f, 1.0f);//orange
-	glVertex2f(0.0f, 100.0f);
-	glColor4f(1.0f, 0.5f, 0.0f, 1.0f);//orange
-	glVertex2f(0.0f, 0.0f);
-	glColor4f(1.0f, 0.5f, 0.0f, 1.0f);//orange
-	glVertex2f(-100.0f, 0.0f);
-	glEnd();
 }
-
-void DrawTriangleRed(void) {
-	glBegin(GL_POLYGON);// OpenGL’a state
-	glColor4f(1.0f, 0.0f, 0.0f, 1.0f);//Red;
-	glVertex2f(0.0f, 100.0f);
-	glColor4f(1.0f, 0.0f, 0.0f, 1.0f);//Red;
-	glVertex2f(0.0f, 0.0f);
-	glColor4f(1.0f, 0.0f, 0.0f, 1.0f);//Red;
-	glVertex2f(100.0f, 0.0f);
-	glEnd();
-}
-
-void DrawTriangleMagenta(void) {
-	glBegin(GL_POLYGON);// OpenGL’a state
-	glColor4f(1.0f, 0.0f, 1.0f, 1.0f);//magenta
-	glVertex2f(0.0f, 100.0f);
-	glColor4f(1.0f, 0.0f, 1.0f, 1.0f);//magenta
-	glVertex2f(0.0f, 0.0f);
-	glColor4f(1.0f, 0.0f, 1.0f, 1.0f);//magenta
-	glVertex2f(-100.0f, 0.0f);
-	glEnd();
-}
-
-void DrawTriangleYellow(void) {
-	glBegin(GL_POLYGON);// OpenGL’a state
-	glColor4f(1.0f, 1.0f, 0.0f, 1.0f);//yellow
-	glVertex2f(0.0f, 100.0f);
-	glColor4f(1.0f, 1.0f, 0.0f, 1.0f);//yellow
-	glVertex2f(0.0f, 0.0f);
-	glColor4f(1.0f, 1.0f, 0.0f, 1.0f);//yellow
-	glVertex2f(100.0f, 0.0f);
-	glEnd();
-}
-*/
 
 
 void MyDisplay(void) {
@@ -116,93 +101,19 @@ void MyDisplay(void) {
 	glShadeModel(GL_SMOOTH);
 	glTranslated(0, 0, 0);
 
-
 	for (int i = 0; i < 4; i++) {
-
-		glPushMatrix();
+	
 		glRotated(i * 90, 0, 0, 100);
-		DrawQuarter();
-		glPopMatrix();
+		QuarterMovment();
+		//DrawQuarter();
 		glLoadIdentity();
-		glPushMatrix();
+		
 	}
 
-//--------------blue triangle----------//
-/*	DrawTriangleBlue();
-	glRotated(90, 0, 0, 100);
-	DrawTriangleBlue();
-	glRotated(180, 0, 0, 100);
-	DrawTriangleBlue();
-	glRotated(270, 0, 0, 100);
-	DrawTriangleBlue();
-
-	//--------------green triangle----------//
-	glTranslated(0, 100, 0);
-	DrawTriangleGreen();
-	glRotated(90, 0, 0, 100);
-	glTranslated(-100, 100, 0);
-	DrawTriangleGreen();
-	glRotated(180, 0, 0, 100);
-	glTranslated(0, 200, 0);
-	DrawTriangleGreen();
-	glRotated(270, 0, 0, 100);
-	glTranslated(100, 100, 0);
-	DrawTriangleGreen();
-	//--------------orange triangle----------//
-	glTranslated(0,0, 0);
-	DrawTriangleOrange();
-	glRotated(90, 0, 0, 100);
-	glTranslated(-100, 100, 0);
-	DrawTriangleOrange();
-	glRotated(180, 0, 0, 100);
-	glTranslated(0, 200, 0);
-	DrawTriangleOrange();
-	glRotated(270, 0, 0, 100);
-	glTranslated(100, 100, 0);
-	DrawTriangleOrange();
-
-	//--------------red triangle----------//
-	glTranslated(0, 100, 0);
-	DrawTriangleRed();
-	glRotated(90, 0, 0, 100);
-	glTranslated(-200, 200, 0);
-	DrawTriangleRed();
-	glRotated(180, 0, 0, 100);
-	glTranslated(0, 400, 0);
-	DrawTriangleRed();
-	glRotated(270, 0, 0, 100);
-	glTranslated( 200, 200, 0);
-	DrawTriangleRed();
-
-	//--------------magenta triangle----------//
-	glTranslated( 0, 0, 0);
-	DrawTriangleMagenta();
-	glRotated(90, 0, 0, 100);
-	glTranslated(-200, 200, 0);
-	DrawTriangleMagenta();
-	glRotated(180, 0, 0, 100);
-	glTranslated(0, 400, 0);
-	DrawTriangleMagenta();
-	glRotated(270, 0, 0, 100);
-	glTranslated(200, 200, 0);
-	DrawTriangleMagenta();
-
-	//--------------yellow triangle----------//
-	glTranslated(100, -100, 0);
-	DrawTriangleYellow();
-	glRotated(90, 0, 0, 100);
-	glTranslated(0, 200, 0);
-	DrawTriangleYellow();
-	glRotated(180, 0, 0, 100);
-	glTranslated(200, 200, 0);
-	DrawTriangleYellow();
-	glRotated(270, 0, 0, 100);
-	glTranslated(200, 0, 0);
-	DrawTriangleYellow();
-	*/
 
 	// The end of scene
 	glFlush();//start processing buffered OpenGL routines
+	glutTimerFunc(20, Timer, 0);
 }
 
 
@@ -212,15 +123,18 @@ void MyInit(void) {
 	glViewport(0, 0, 400, 400);//window origin and size
 	glMatrixMode(GL_PROJECTION);//
 	glLoadIdentity();//=1
-	gluOrtho2D(-500.0, 500.0,-500.0, 500.0);
+	gluOrtho2D(-500.0, 500.0, -500.0, 500.0);
 	glMatrixMode(GL_MODELVIEW);
 	glLoadIdentity();//=1
 }
 
+
+
+
 int main(int argc, char** argv) { //<- for normal API
 	glutInit(&argc, argv);
 	glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);//single buffer and RGBA
-	glutInitWindowSize(800, 800);//initial window size
+	glutInitWindowSize(600, 600);//initial window size
 	glutInitWindowPosition(100, 100);
 	glutCreateWindow("Triangles");//create widnow, hello title bar
 	MyInit();
